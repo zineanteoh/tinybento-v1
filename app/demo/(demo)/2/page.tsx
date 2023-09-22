9d77b2319730cba7dc05f033e85c71fe4082c730
jacoblurie29 <jacoblurie29@gmail.com>
2023-09-22 16:03:26 -0500

    add demo 2

--- app/demo/(demo)/2/page.tsx
+++ app/demo/(demo)/2/page.tsx
@@ -1,79 +1,18 @@
 "use client";
-
-import React, { useState } from "react";
+import React from "react";
 import styles from "./page.module.css";
-import {
-  DndContext,
-  DragEndEvent,
-  closestCenter,
-  useDraggable,
-  useDroppable,
-} from "@dnd-kit/core";
-import { CSS } from "@dnd-kit/utilities";
 
-const MoveableSquare = () => {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: "draggable",
-  });
-
-  const style = transform
-    ? {
-        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-      }
-    : undefined;
-
-  return (
-    <div
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-      style={style}
-      className={styles.square}
-    >
-      IM JACOB
-    </div>
-  );
-};
-
-const DroppableSquare = (props: { children: React.ReactNode }) => {
-  // use droppable
-  const { isOver, setNodeRef } = useDroppable({
-    id: "droppable",
-  });
-
-  const style = {
-    padding: 20,
-    backgroundColor: isOver ? "green" : "red",
-  };
-
-  return (
-    <div ref={setNodeRef} style={style} className={styles.square}>
-      {props.children}
-    </div>
-  );
-};
+import DroppableBento from "@/components/demo-1/DroppableBento";
+import { useStore } from "@/store/store";
 
 const Demo2 = () => {
-  const [isDropped, setIsDropped] = useState(false);
-
-  const onDragEnd = (event: DragEndEvent) => {
-    if (event.over && event.over.id === "droppable") {
-      setIsDropped(true);
-    }
-  };
+  const { isDropped, droppedIngredients } = useStore();
 
   return (
-    <div className={styles.container}>
-      <DndContext onDragEnd={onDragEnd}>
-        <div className={styles.bentoSpace}>
-          <DroppableSquare>
-            {isDropped ? <MoveableSquare /> : "Drop Here"}
-          </DroppableSquare>
-        </div>
-        <div className={styles.sidebar}>
-          {!isDropped ? <MoveableSquare /> : null}
-        </div>
-      </DndContext>
+    <div className={styles.canvas}>
+      <DroppableBento>
+        {isDropped ? `DROPPED: ${droppedIngredients}` : "EMPTY"}
+      </DroppableBento>
     </div>
   );
 };
