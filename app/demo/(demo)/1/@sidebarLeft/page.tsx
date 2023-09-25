84c2bfd5daaf8e5c4d82e35294f320d86097482f
Zi Nean Teoh <zinean00@gmail.com>
2023-09-25 14:48:52 -0500

    enhance left sidebar

--- app/demo/(demo)/1/@sidebarLeft/page.tsx
+++ app/demo/(demo)/1/@sidebarLeft/page.tsx
@@ -17,31 +17,58 @@ const SidebarLeft = () => {
         alignItems: "left",
         paddingLeft: "10px",
         paddingTop: "25px",
+        gap: "10px",
       }}
     >
       <div>
         dragging: {dragging ? `${dragging.width}x${dragging.height}` : ""}
       </div>
-      <div>
-        bentoIngredients:
-        {bentoIngredients.map((ingredient, i) => {
-          const { x, y } = ingredient.coordinate;
 
-          return (
-            <div key={i}>
-              {/* ({x}, {y}) {ingredient.id} */}
-              {JSON.stringify(ingredient)}
-            </div>
-          );
-        })}
-      </div>
       <div>
         bentoIngredients2D:
-        {JSON.stringify(
-          bentoIngredients2D.map((row) =>
-            row.map((col) => (col ? `${col.height}x${col.width}` : null))
-          )
-        )}
+        <div>
+          {/* row and cols of bentoIngredients2d */}
+          {bentoIngredients2D.map((row, rowIndex) => (
+            <div
+              key={rowIndex}
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              {row.map((col, colIndex) => (
+                <span key={colIndex}>
+                  {col ? `[${col.height}x${col.width}]` : <div>[null]</div>}
+                </span>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        bentoIngredients:
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          {bentoIngredients
+            .map((ingredient) => {
+              const { width, height } = ingredient;
+              const { x, y } = ingredient.coordinate;
+              const variant = ingredient.variant;
+              return `${height}x${width}, (${x},${y}), ${variant}`;
+            })
+            .map((data, index) => {
+              return (
+                <span key={index}>
+                  {index}: {data}
+                </span>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
