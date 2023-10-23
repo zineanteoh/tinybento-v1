34a39372fb6a71fbbca9a04112662211c7196194
Zi Nean Teoh <zinean00@gmail.com>
2023-10-23 01:16:33 -0500

    add KitchenHeader story

---
+++ stories/KitchenHeader.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import KitchenHeader from "../components/kitchen/header/KitchenHeader";
+
+const meta: Meta<typeof KitchenHeader> = {
+  component: KitchenHeader,
+};
+export default meta;
+type Story = StoryObj<typeof KitchenHeader>;
+
+export const Default: Story = {
+  args: {},
+};
