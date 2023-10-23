dff07bc704205c3352528c8827f73c72664bceb2
Zi Nean Teoh <zinean00@gmail.com>
2023-10-23 01:09:09 -0500

    add HeaderProfile story

---
+++ stories/HeaderProfile.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import HeaderProfile from "../components/kitchen/header/HeaderProfile";
+
+const meta: Meta<typeof HeaderProfile> = {
+  component: HeaderProfile,
+};
+export default meta;
+type Story = StoryObj<typeof HeaderProfile>;
+
+export const Jacob: Story = {
+  args: {
+    userName: "Jacob Lurie",
+    bentoName: "Jacob's Jolly Jamboree",
+  },
+};
+
+export const William: Story = {
+  args: {
+    userName: "William Wu",
+    bentoName: "William's Wonderful World of Wonders",
+  },
+};
+
+export const Zi: Story = {
+  args: {
+    userName: "Zi Teoh",
+    bentoName: "My Life in a Bentobox",
+  },
+};
