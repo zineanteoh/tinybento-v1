7c0047eaba4abfef0f7d84b8db16a72a04e31907
Zi Nean Teoh <zinean00@gmail.com>
2023-10-24 00:57:48 -0500

    draft layout for kitchen

--- app/kitchen/page.tsx
+++ app/kitchen/page.tsx
@@ -3,15 +3,54 @@ import React from "react";
 
 import styles from "./page.module.css";
 import KitchenHeader from "@/components/kitchen/header/KitchenHeader";
+import ActionButton from "@/components/kitchen/ActionButton";
+import {
+  IconChangeTheme,
+  IconEdit,
+  IconHierarchy,
+  IconPlus,
+  IconShare,
+} from "@/utils/iconLibrary";
 
 const Kitchen = () => {
   return (
     <div className={styles.container}>
       <KitchenHeader />
 
-      {/* TODO: add left side bar */}
+      <div className={styles.content}>
+        <div className={styles.side}>
+          <ActionButton
+            actionName="Add Ingredient"
+            color="#FFCBA6"
+            icon={IconPlus}
+          />
+          <ActionButton
+            actionName="Edit Content"
+            color="#A8FFB1"
+            icon={IconEdit}
+          />
+          <ActionButton
+            actionName="Hierarchy"
+            color="#AFB7FF"
+            icon={IconHierarchy}
+          />
+          <ActionButton
+            actionName="Share Bento"
+            color="#FED6FF"
+            icon={IconShare}
+          />
+          <ActionButton
+            actionName="Change Theme"
+            color="#C0FBFF"
+            icon={IconChangeTheme}
+          />
 
-      {/* TODO: add bento box */}
+          {/* TODO: add ActionContainer here */}
+        </div>
+        <div className={styles.main}>
+          {/* TODO: place Bento here, touching the very left side of main */}
+        </div>
+      </div>
     </div>
   );
 };
