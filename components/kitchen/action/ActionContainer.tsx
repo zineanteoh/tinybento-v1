a23a4bab799912198f3263b3c68ae8516da1ba31
Zi Nean Teoh <zinean00@gmail.com>
2023-10-24 15:58:44 -0500

    implement ActionContainer

---
+++ components/kitchen/action/ActionContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ActionContainer.module.css";
+
+const ActionContainer = () => {
+  return <div className={styles.container}>ActionContainer</div>;
+};
+
+export default ActionContainer;
