6375ff8536f2dc896847c3c35064eeb28afd2767
Zi Nean Teoh <zinean00@gmail.com>
2023-10-24 17:06:37 -0500

    show mobile not available on mobile screen

--- app/kitchen/layout.tsx
+++ app/kitchen/layout.tsx
@@ -1,5 +1,16 @@
+import styles from "./layout.module.css";
+
 const KitchenLayout = (props: { children: React.ReactNode }) => {
-  return <div>{props.children}</div>;
+  return (
+    <div className={styles.layout}>
+      {props.children}
+
+      {/* Render mobile not available */}
+      <div className={`bold ${styles.mobile}`}>
+        TinyBento is currently only available on Desktop
+      </div>
+    </div>
+  );
 };
 
 export default KitchenLayout;
