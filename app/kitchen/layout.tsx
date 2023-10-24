b5d74bfd4d30352851254cd6ffeb9b8e9021098f
Zi Nean Teoh <zinean00@gmail.com>
2023-10-24 01:02:20 -0500

    rename to KitchenLayout

--- app/kitchen/layout.tsx
+++ app/kitchen/layout.tsx
@@ -2,8 +2,8 @@
 
 // READ: https://nextjs.org/docs/app/building-your-application/routing/route-groups
 // this layout will be shared by all pages in the "(demo)" Route Group
-const DemoLayout = (props: { children: React.ReactNode }) => {
+const KitchenLayout = (props: { children: React.ReactNode }) => {
   return <div>{props.children}</div>;
 };
 
-export default DemoLayout;
+export default KitchenLayout;
