0c87884c5afde79d5820d9757151082c22155f11
Zi Nean Teoh <zinean00@gmail.com>
2023-10-24 15:58:35 -0500

    improve page layout

--- app/kitchen/page.tsx
+++ app/kitchen/page.tsx
@@ -11,6 +11,7 @@ import {
   IconPlus,
   IconShare,
 } from "@/utils/iconLibrary";
+import ActionContainer from "@/components/kitchen/action/ActionContainer";
 
 const Kitchen = () => {
   return (
@@ -19,33 +20,38 @@ const Kitchen = () => {
 
       <div className={styles.content}>
         <div className={styles.side}>
-          <ActionButton
-            actionName="Add Ingredient"
-            color="#FFCBA6"
-            icon={IconPlus}
-          />
-          <ActionButton
-            actionName="Edit Content"
-            color="#A8FFB1"
-            icon={IconEdit}
-          />
-          <ActionButton
-            actionName="Hierarchy"
-            color="#AFB7FF"
-            icon={IconHierarchy}
-          />
-          <ActionButton
-            actionName="Share Bento"
-            color="#FED6FF"
-            icon={IconShare}
-          />
-          <ActionButton
-            actionName="Change Theme"
-            color="#C0FBFF"
-            icon={IconChangeTheme}
-          />
+          <div className={styles.actionButtons}>
+            <ActionButton
+              actionName="Add Ingredient"
+              color="#FFCBA6"
+              icon={IconPlus}
+            />
+            <ActionButton
+              actionName="Edit Content"
+              color="#A8FFB1"
+              icon={IconEdit}
+            />
+            <ActionButton
+              actionName="Hierarchy"
+              color="#AFB7FF"
+              icon={IconHierarchy}
+            />
+            <ActionButton
+              actionName="Share Bento"
+              color="#FED6FF"
+              icon={IconShare}
+            />
+            <ActionButton
+              actionName="Change Theme"
+              color="#C0FBFF"
+              icon={IconChangeTheme}
+            />
+          </div>
 
           {/* TODO: add ActionContainer here */}
+          <div className={styles.actionContainer}>
+            {/* <ActionContainer /> */}
+          </div>
         </div>
         <div className={styles.main}>
           {/* TODO: place Bento here, touching the very left side of main */}
