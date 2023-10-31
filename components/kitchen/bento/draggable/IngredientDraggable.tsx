d387fdc037946fe49b1e20f5e1a86a9bc4e7f5a3
Zi Nean Teoh <zinean00@gmail.com>
2023-10-30 22:44:42 -0500

    migrate IngredientDraggable
    Co-authored-by: Jacob Lurie <jacoblurie29@users.noreply.github.com>

---
+++ components/kitchen/bento/draggable/IngredientDraggable.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./IngredientDraggable.module.css";
+import { useDraggable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
+
+const DraggableIngredient = (props: { content: string }) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: props.content,
+  });
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
+
+  return (
+    <div
+      className={`${styles.draggable} no-select`}
+      ref={setNodeRef}
+      style={style}
+      {...listeners}
+      {...attributes}
+    >
+      {props.content}
+    </div>
+  );
+};
+
+export default DraggableIngredient;
