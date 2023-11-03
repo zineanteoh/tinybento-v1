9328ce5463c5ff5bb50f835a522b36d5d800e05e
Zi Nean Teoh <zinean00@gmail.com>
2023-11-02 19:51:20 -0500

    refactor ingredient-id-to-icon type

--- components/kitchen/action/items/AddIngredients.tsx
+++ components/kitchen/action/items/AddIngredients.tsx
@@ -14,63 +14,30 @@ import {
 } from "@/utils/iconLibrary";
 import AddIngredientDraggable from "@/components/kitchen/bento/draggable/AddIngredientDraggable";
 
-interface IngredientIconType {
-  id: string;
-  icon: React.ReactNode;
+interface IngredientIdToIconType {
+  [key: string]: React.ReactNode;
 }
 
-const ingredients: IngredientIconType[] = [
-  {
-    id: "1x1",
-    // icon: IconIngredient1x1,
-    icon: <div>1x1</div>,
-  },
-  {
-    id: "2x2",
-    // icon: IconIngredient2x2,
-    icon: <div>2x2</div>,
-  },
-  {
-    id: "1x2",
-    // icon: IconIngredient1x2,
-    icon: <div>1x2</div>,
-  },
-  {
-    id: "2x3",
-    // icon: IconIngredient2x3,
-    icon: <div>2x3</div>,
-  },
-  {
-    id: "1x3",
-    // icon: IconIngredient1x3,
-    icon: <div>1x3</div>,
-  },
-  {
-    id: "2x4",
-    // icon: IconIngredient2x4,
-    icon: <div>2x4</div>,
-  },
-  {
-    id: "1x4",
-    // icon: IconIngredient1x4,
-    icon: <div>1x4</div>,
-  },
-  {
-    id: "3x3",
-    // icon: IconIngredient3x3,
-    icon: <div>3x3</div>,
-  },
-];
+const ingredients: IngredientIdToIconType = {
+  "1x1": IconIngredient1x1,
+  "2x2": IconIngredient2x2,
+  "1x2": IconIngredient1x2,
+  "2x3": IconIngredient2x3,
+  "1x3": IconIngredient1x3,
+  "2x4": IconIngredient2x4,
+  "1x4": IconIngredient1x4,
+  "3x3": IconIngredient3x3,
+};
 
 const AddIngredients = () => {
   return (
     <AnimateRightLeft>
       <ActionContainer>
         <div className={styles.container}>
-          {ingredients.map((ingredient) => (
-            <div className={styles.miniContainer} key={ingredient.id}>
-              <AddIngredientDraggable key={ingredient.id} id={ingredient.id}>
-                {ingredient.id}
+          {Object.entries(ingredients).map(([id, icon]) => (
+            <div className={styles.miniContainer} key={id}>
+              <AddIngredientDraggable key={id} id={id}>
+                {icon}
               </AddIngredientDraggable>
             </div>
           ))}
